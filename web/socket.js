c035c9c63f3739c74c8e46e47fa06121e79beedc
alessmolt <alessando7.molteni@mail.polimi.it>
2022-01-10 11:59:23 +0100

    work on lights cont'd

--- web/socket.js
+++ web/socket.js
@@ -1,16 +1,16 @@
 //HTML OBJ
-let light = document.getElementById('dirLight');
-let spotlight = document.getElementById('spotLight');
+let light = document.getElementsByClassName("primaryLight");
+let secondarylight = document.getElementsByClassName("secondaryLight");
 
 //INSERT HERE YOUR MACHINE'S IPv4 ADDRESS
 let oscPort = new osc.WebSocketPort({
-  url: 'wss://192.168.1.192:3000',
+  url: "wss://192.168.1.208:3000",
 });
 
-oscPort.on('message', function (msg) {
-  console.log('message', msg);
+oscPort.on("message", function (msg) {
+  console.log("message", msg);
   switch (msg.address) {
-    case '/RGB':
+    case "/RGB":
       colorOnMsg(msg.args);
       break;
 
@@ -29,41 +29,45 @@ oscPort.open();
  */
 
 function rgbToHex(r, g, b) {
-  let res = '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  let res = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
   return res;
 }
 
 function componentToHex(c) {
   var hex = c.toString(16);
-  return hex.length == 1 ? '0' + hex : hex;
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
 function colorOnMsg(rgb) {
   let hexC = rgbToHex(rgb[0], rgb[1], rgb[2]);
   let compHex = getComplementaryColor(hexC);
-  console.log('hex', hexC);
-  console.log('complementary', compHex);
-  light.setAttribute('animation', {
-    property: 'light.color',
-    to: hexC,
-    dur: 500,
-    easing: 'linear',
-  });
-  spotlight.setAttribute('animation', {
-    property: 'light.color',
-    to: compHex,
-    dur: 500,
-    easing: 'linear',
-  });
+  console.log("hex", hexC);
+  console.log("complementary", compHex);
+  for (let i = 0; i < light.length; i++) {
+    light[i].setAttribute("animation", {
+      property: "light.color",
+      to: hexC,
+      dur: 500,
+      easing: "linear",
+    });
+  }
+  for (let j = 0; j < secondarylight.length; j++) {
+    secondarylight[j].setAttribute("animation", {
+      property: "light.color",
+      to: compHex,
+      dur: 500,
+      easing: "linear",
+    });
+  }
   //light.setAttribute('light', 'color', hexC);
 }
 
-const getComplementaryColor = (color = '') => {
+const getComplementaryColor = (color = "") => {
   const colorPart = color.slice(1);
   const ind = parseInt(colorPart, 16);
   let iter = ((1 << (4 * colorPart.length)) - 1 - ind).toString(16);
   while (iter.length < colorPart.length) {
-    iter = '0' + iter;
+    iter = "0" + iter;
   }
-  return '#' + iter;
+  return "#" + iter;
 };
