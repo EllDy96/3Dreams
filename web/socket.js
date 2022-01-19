0aedfc931224a9d038bf7587b1115211975cfec7
alessmolt <alessando7.molteni@mail.polimi.it>
2022-01-19 17:28:00 +0100

    fixed primary light

--- web/socket.js
+++ web/socket.js
@@ -1,22 +1,22 @@
 //HTML OBJ
-let light = document.getElementsByClassName('primaryLight');
-let secondarylight = document.getElementsByClassName('secondaryLight');
-let sky = document.getElementById('sky');
-let scene = document.getElementById('scene');
-let colorAnimation = document.getElementsByClassName('colorAnimation');
+let light = document.getElementsByClassName("primaryLight");
+let secondarylight = document.getElementsByClassName("secondaryLight");
+let sky = document.getElementById("sky");
+let scene = document.getElementById("scene");
+let colorAnimation = document.getElementsByClassName("colorAnimation");
 
 //INSERT HERE YOUR MACHINE'S IPv4 ADDRESS
 let oscPort = new osc.WebSocketPort({
-  url: 'wss://192.168.1.192:3000',
+  url: "wss://192.168.1.208:3000",
 });
 
-oscPort.on('message', function (msg) {
+oscPort.on("message", function (msg) {
   switch (msg.address) {
-    case '/RGB':
+    case "/RGB":
       colorOnMsg(msg.args);
       break;
-    case '/ENERGY':
-      console.log('message', msg);
+    case "/ENERGY":
+      console.log("message", msg);
       lightIntensity(msg.args);
       break;
 
@@ -37,11 +37,11 @@ oscPort.open();
 function lightIntensity(args) {
   let intensity = args[0];
   for (let j = 0; j < secondarylight.length; j++) {
-    secondarylight[j].setAttribute('animation__light', {
-      property: 'light.intensity',
+    secondarylight[j].setAttribute("animation__light", {
+      property: "light.intensity",
       to: intensity,
       dur: 500,
-      easing: 'linear',
+      easing: "linear",
     });
 
     /* secondarylight[j].object3D.el.getAttribute('light').intensity = intensity; */
@@ -49,28 +49,28 @@ function lightIntensity(args) {
 }
 
 function rgbToHex(r, g, b) {
-  let res = '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  let res = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
   return res;
 }
 
 function componentToHex(c) {
   var hex = c.toString(16);
-  return hex.length == 1 ? '0' + hex : hex;
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
 function hexToRgbA(hex) {
   var c;
   if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-    c = hex.substring(1).split('');
+    c = hex.substring(1).split("");
     if (c.length == 3) {
       c = [c[0], c[0], c[1], c[1], c[2], c[2]];
     }
-    c = '0x' + c.join('');
+    c = "0x" + c.join("");
     return (
-      'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',1)'
+      "rgba(" + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(",") + ",1)"
     );
   }
-  throw new Error('Bad Hex');
+  throw new Error("Bad Hex");
 }
 
 function colorOnMsg(rgb) {
@@ -80,47 +80,47 @@ function colorOnMsg(rgb) {
   let compRgba = hexToRgbA(compHex);
 
   /* console.log('hex', hexC); */
-  console.log('complementary', compRgba);
+  console.log("complementary", compRgba);
 
-  scene.setAttribute('animation', {
-    property: 'fog.color',
+  scene.setAttribute("animation", {
+    property: "fog.color",
     to: hexC,
-    dur: 500,
-    easing: 'linear',
+    dur: 1500,
+    easing: "linear",
   });
 
   for (let i = 0; i < light.length; i++) {
     /* light[i].object3D.el.getAttribute('light').color = hexC; */
 
-    light[i].setAttribute('animation', {
-      property: 'light.color',
+    light[i].setAttribute("animation", {
+      property: "light.color",
       to: hexC,
-      dur: 500,
-      easing: 'linear',
+      dur: 1500,
+      easing: "linear",
     });
   }
   for (let j = 0; j < secondarylight.length; j++) {
     /* secondarylight[j].object3D.el.getAttribute('light').color = compHex; */
-    secondarylight[j].setAttribute('animation', {
-      property: 'light.color',
+    secondarylight[j].setAttribute("animation", {
+      property: "light.color",
       to: compHex,
-      dur: 500,
-      easing: 'linear',
+      dur: 1500,
+      easing: "linear",
     });
   }
 }
 
-const getComplementaryColor = (color = '') => {
+const getComplementaryColor = (color = "") => {
   const colorPart = color.slice(1);
   const ind = parseInt(colorPart, 16);
   let iter = ((1 << (4 * colorPart.length)) - 1 - ind).toString(16);
   while (iter.length < colorPart.length) {
-    iter = '0' + iter;
+    iter = "0" + iter;
   }
-  return '#' + iter;
+  return "#" + iter;
 };
 
 setInterval(() => {
-  console.log(secondarylight[0].getAttribute('light').color);
-  console.log(secondarylight[0].getAttribute('light').intensity);
+  console.log(secondarylight[0].getAttribute("light").color);
+  console.log(secondarylight[0].getAttribute("light").intensity);
 });
